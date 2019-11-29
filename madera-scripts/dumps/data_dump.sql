aa110fc4411c5bc35826238916a4002ffdde0cbb
BoiteSphinx <boite.sphinx@gmail.com>
2019-11-29 09:55:17 +0100

    [SQL] minor fix
    - Changed password for TEST.USER to lowercase

--- madera-scripts/dumps/data_dump.sql
+++ madera-scripts/dumps/data_dump.sql
@@ -12,7 +12,7 @@ INSERT INTO madera.utilisateur(i_utilisateur_id, v_nom, v_prenom, v_mail, v_tel,
 
 
 INSERT INTO madera.utilisateur(i_utilisateur_id, v_nom, v_prenom, v_mail, v_tel, i_role_id, v_login, v_password) VALUES
-(4, 'TEST', 'USER', 'user@test.com', '0000000000', 3, 'TEST.USER', '8D969EEF6ECAD3C29A3A629280E686CF0C3F5D5A86AFF3CA12020C923ADC6C92');
+(4, 'TEST', 'USER', 'user@test.com', '0000000000', 3, 'TEST.USER', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92');
 
 SELECT setval('madera.utilisateur_i_utilisateur_id_seq'::regclass, 3);
 
