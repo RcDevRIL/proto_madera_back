a5c9d4da549ac421a110b9e67b2069efe13746bd
Fabien <fabien@linux-2.home>
2020-01-01 23:21:37 +0100

    [InstallDb] - Modification du script install_db.sh (depend de la config)

--- madera-scripts/install_db.sh
+++ madera-scripts/install_db.sh
@@ -7,15 +7,15 @@ echo "---------------------------------------------------"
 echo "Attention s'il faut un mot de passe pour votre base de données il faudra le renseigner !"
 
 echo "Suppression de l' ancience base de données"
-psql -U postgres -h localhost madera -W -c "DROP SCHEMA madera CASCADE;"
+psql -U postgres madera -W -c "DROP SCHEMA madera CASCADE;"
 
 echo "Création de la nouvelle base de données"
-psql -U postgres -h localhost madera -W -f dumps/madera_dump.sql
+psql -U postgres madera -W -f dumps/madera_dump.sql
 
 echo "Félicitations, votre base de données est à jour !"
 echo "---------------------"
 echo "Insertion des données"
 echo "---------------------"
-psql -U postgres -h localhost madera -W -f dumps/data_dump.sql
+psql -U postgres madera -W -f dumps/data_dump.sql
 
 echo "La base de données est opérationnel !"
