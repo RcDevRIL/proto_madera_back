75f227bc38df7b7fea565781f70a9eaf4ae15afd
Romain <r.chevallier21@hotmail.fr>
2020-01-04 22:49:26 +0100

    update script de déploiement

--- madera-scripts/deploy_backend.sh
+++ madera-scripts/deploy_backend.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+echo "--------------------------"
+echo "Mise à jour du code source"
+echo "--------------------------"
+
+cd ..
+git pull
+
 echo "---------------------------------------------------"
 echo "Création / Mise à jour de la base de données madera"
 echo "---------------------------------------------------"
@@ -10,12 +17,19 @@ echo "Suppression de l' ancience base de données"
 psql -U postgres madera -W -c "DROP SCHEMA madera CASCADE;"
 
 echo "Création de la nouvelle base de données"
-psql -U postgres madera -W -f dumps/madera_dump.sql
+psql -U postgres madera -W -f madera-scripts/dumps/madera_dump.sql
 
 echo "Félicitations, votre base de données est à jour !"
 echo "---------------------"
 echo "Insertion des données"
 echo "---------------------"
-psql -U postgres madera -W -f dumps/data_dump.sql
+psql -U postgres madera -W -f madera-scripts/dumps/data_dump.sql
 
 echo "La base de données est opérationnel !"
+
+
+echo "--------------------------"
+echo "Lancement de l'application"
+echo "--------------------------"
+
+#mvn spring-boot:run
