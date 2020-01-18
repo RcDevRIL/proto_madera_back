b57b42f1f951d35e98d608b0edeb1653a8974d9f
Romain <r.chevallier21@hotmail.fr>
2020-01-18 22:21:44 +0100

    [Web Support] adding cors configuration
    - added to enable frontend api calls from web browsers

--- src/main/java/com/madera/api/security/SecurityConfig.java
+++ src/main/java/com/madera/api/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.madera.api.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,10 +9,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Classe permettant de configurer l'authentification
- * 
+ *
  * @author LADOUCE Fabien, CHEVALLIER Romain, HELIOT David
  * @version 1.0-RELEASE
  */
@@ -28,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     /**
      * Configure les accès aux différentes routes de l'application
-     * 
+     *
      * @param http HttpSecurity
      * @throws Exception exception
      */
@@ -37,15 +44,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         // We don't need CSRF for this example
         http.csrf().disable().httpBasic().disable().formLogin().disable()
+                //adding a cors configuration using by default corsConfigurationSource
+                .cors().and()
                 // dont authenticat this particular requests
                 .authorizeRequests().antMatchers("/api/authentification", "/api").permitAll().
                 // all other requests need to be authenticated
-                anyRequest().authenticated().and()
+                        anyRequest().authenticated().and()
                 // make sure we use stateless session; session won't be used to
                 // store user's state.
                 .exceptionHandling().authenticationEntryPoint(jwtAuthentificationEntryPoint).and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         // Add a filter to validate the tokens with every request
         http.addFilterBefore(authentificationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration conf = new CorsConfiguration();
+        conf.setAllowedHeaders(Collections.singletonList("*"));
+        conf.setAllowedMethods(Collections.singletonList("*"));
+        conf.setMaxAge((long) 3600);
+        conf.setAllowCredentials(true);
+        //conf.setExposedHeaders();
+        conf.setAllowedOrigins(Arrays.asList("https://rcdevril.github.io/proto_madera_front/", "https://proto_madera_front.codemagic.app/"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", conf);
+        return source;
     }
 }
